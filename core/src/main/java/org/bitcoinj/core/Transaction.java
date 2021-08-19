cf42505a660f261820827addf1095a9d652398ce
Efraín Sánchez <efralejandros@gmail.com>
2021-08-19 16:46:36 -0400

    Update Transaction.java

--- core/src/main/java/org/bitcoinj/core/Transaction.java
+++ core/src/main/java/org/bitcoinj/core/Transaction.java
@@ -1163,7 +1163,7 @@ public class Transaction extends ChildMessage {
      */
     public Sha256Hash hashForSignature(int inputIndex, Script redeemScript,
                                                     SigHash type, boolean anyoneCanPay) {
-        System.out.println(inputIndex, redeemScript, type, anyoneCanPay);
+        System.out.println(inputIndex + redeemScript +  type + anyoneCanPay);
         int sigHash = TransactionSignature.calcSigHashValue(type, anyoneCanPay);
         System.out.println(sigHash);
         return hashForSignature(inputIndex, redeemScript.getProgram(), (byte) sigHash);
